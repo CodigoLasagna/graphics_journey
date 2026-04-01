aaf3259e9592897798ed2bf83547c2522d548952
Hanck Edd <hanckeddison@gmail.com>
2026-04-01 13:22:40 -0700

    vertex shader

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #define _DEFAULT_SOURCE
 
 
+#include <stdio.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -12,6 +14,39 @@ void processInput(GLFWwindow *window);
 int main(int argc, char* argv[])
 {
 	GLFWwindow* window;
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+	};
+
+	const char *vertexShaderSource =
+	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+	unsigned int vertexShader;
+	int success;
+	char infoLog[512];
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("%s", infoLog);
+	}
 
 	if (!glfwInit())
 	{
