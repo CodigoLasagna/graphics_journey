758777f84a743615634e4a0576c180b5cadcaa58
Hanck Edd <hanckeddison@gmail.com>
2026-04-01 13:13:58 -0700

    window with colored background

--- main.c
+++ main.c
@@ -2,26 +2,16 @@
 #define _DEFAULT_SOURCE
 
 
-#include <stdio.h>
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 
 int main(int argc, char* argv[])
 {
 	GLFWwindow* window;
-	float vertices[] =
-	{
-		-0.5f, 0.0f,
-		0.5f, 0.0f
-	};
-	GLuint VBO, VAO;
-
-	/*shaders minimos*/
-	const char* vertexShadersSrc =
-		"#version 330 core\n"
-		"layout";
 
 	if (!glfwInit())
 	{
@@ -36,25 +26,17 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	/*contexto de opengl usando glfw*/
 	glfwMakeContextCurrent(window);
-
-	/*context for drawing line*/
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	/*atributos de posición*/
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glViewport(0, 0, 500, 500);
 
 
 
 	while(!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
@@ -65,4 +47,17 @@ int main(int argc, char* argv[])
 	glfwTerminate();
 
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
 }
